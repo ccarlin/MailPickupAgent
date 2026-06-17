86fb39f3c3f0798cd712c75c1c845aacc274862b
Chuck Carlin <ccarlinx@hotmail.com>
2026-06-16 20:21:27 -0500

    Added preview for deleted emails to help decide if you should really recover them.

--- routes/emailPreview.js
+++ routes/emailPreview.js
@@ -6,8 +6,10 @@ const config = require('../config');
 /* GET Thumbnail images. */
 router.get('/', async function(req, res) {
     
-    let mailKey = req.query.mailKey;
+    let mailKey = req.query.mailKey;    
     let mailFile = config.QUARANTINE_DIR + "\\" + mailKey + ".MAI";
+    if (req.query.isDeleted)
+        mailFile = config.DELETED_DIR + "\\" + mailKey + ".MAI";
     let inHTML = (req.query.view && req.query.view == "HTML");
     let mailMessage = await tools.emailExtract(mailFile, inHTML);
     res.send(mailMessage);    
